3fe09f67b0c2e528474c758801fb0e551ab7b71a
RohanUzumaki <rohan.rop01@gmail.com>
2023-04-24 15:31:53 +0530

    New App Working

--- app/src/main/java/com/example/horizonapp/HomeActivity.java
+++ app/src/main/java/com/example/horizonapp/HomeActivity.java
@@ -2,19 +2,27 @@ package com.example.horizonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 
 public class HomeActivity extends AppCompatActivity {
-    Button bookBtn;
-    Button consBtn;
+    Button btnBook;
+    Button btnOnlineCons;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnBook = findViewById(R.id.bookBtn);
+        btnOnlineCons = findViewById(R.id.consBtn);
+
+        btnBook.setOnClickListener(task -> startActivity(new Intent(HomeActivity.this, DocListActivity.class)));
+        btnOnlineCons.setOnClickListener(task -> startActivity(new Intent(HomeActivity.this, OnlineConsActivity.class)));
     }
+
 }
