b3447e4e9ec4f229ac0843a7ee325e1ba4115027
RohanUzumaki <rohan.rop01@gmail.com>
2023-04-25 12:41:24 +0530

    New App Working

--- app/src/main/java/com/example/horizonapp/OnlineConsActivity.java
+++ app/src/main/java/com/example/horizonapp/OnlineConsActivity.java
@@ -10,5 +10,7 @@ public class OnlineConsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_cons);
+
+
     }
 }
