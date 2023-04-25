079d48f260156aedd77f22b94b9ef1a4b5ec9362
RohanUzumaki <rohan.rop01@gmail.com>
2023-04-25 19:30:22 +0530

    New App Working

--- app/src/main/java/com/example/horizonapp/OnlineConsActivity.java
+++ app/src/main/java/com/example/horizonapp/OnlineConsActivity.java
@@ -2,15 +2,23 @@ package com.example.horizonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class OnlineConsActivity extends AppCompatActivity {
 
+    Button btnVideoCons;
+    Button btnChatDoc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_cons);
+        btnVideoCons = findViewById(R.id.btnVideo);
+        btnChatDoc = findViewById(R.id.btnChat);
 
+        btnVideoCons.setOnClickListener(task -> startActivity(new Intent(OnlineConsActivity.this, VideoConsultation.class)));
+        btnChatDoc.setOnClickListener(task -> startActivity(new Intent(OnlineConsActivity.this, ChatWithDoc.class)));
 
     }
 }
---
+++ app/src/main/java/com/example/horizonapp/VideoConsultation.java
@@ -0,0 +1,14 @@
+package com.example.horizonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VideoConsultation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_consultation);
+    }
+}
